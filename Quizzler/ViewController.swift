d8ebaaa6f17f09261c7523df2c04ef267940b1d1
Cao Do <gusdocat@student.gu.se>
2018-11-30 16:22:37 +0100

    code cleaning/ Added more UI / UI bugs fixed

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
+    // Variables
     
     let questions = QuestionBank()   // constant
+    var currentQuestion : Question = Question(text: " ", correctAnswer: false)
+    var questionIndex : Int = 0
     var pickedAnswer : Bool = false // Variable
-    var questionNr : Int = 0
     var currentPoint : Int = 0
 
-
-    
+    // Buttons
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
@@ -26,60 +26,70 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let currentQuestion = questions.list[questionNr]
-        questionLabel.text = currentQuestion.questionText
-        scoreLabel.text = String(0)
-        
+        updateUI()
     }
 
-
+    
     @IBAction func answerPressed(_ sender: AnyObject) {
         if sender.tag == 1 {
             pickedAnswer = true
         } else {
             pickedAnswer = false
         }
-        checkAnswer()
-        updateUI()
-
+        update()
+        printLog()
     }
-    
-    
-    
-    func updateUI() {
-        scoreLabel.text = String(currentPoint)
-        if (questionNr < questions.getSize()) {
-            nextQuestion()
-        }else {
-            startOver()
-        }
-    }
-    
-
-    func nextQuestion() {
-        questionNr += 1
-        questionLabel.text = questions.list[questionNr].questionText
-    }
-    
     
     func checkAnswer() {
-        let firstQuestion = questions.list[0]
-        if (firstQuestion.answer == pickedAnswer) {
+        currentQuestion = questions.list[questionIndex]
+        if (currentQuestion.answer == pickedAnswer) {
             currentPoint += 1
-            print("yay")
-        }else {
-            print("yuke")
         }
-        questionNr += 1
     }
     
+    func update() {
+        if (questionIndex < questions.getSize()-1) {
+            nextQuestion()
+        }else {
+            popUp()
+        }
+    }
+    
+    func updateUI() {
+        currentQuestion = questions.list[questionIndex]
+        questionLabel.text = currentQuestion.questionText
+        scoreLabel.text = String(currentPoint)
+        progressLabel.text = "\(questionIndex + 1) / \(questions.getSize())"
+        progressBar.frame.size.width = (view.frame.size.width / CGFloat(questions.getSize())) * CGFloat(questionIndex+1)
+        
+    }
+    
+    func nextQuestion() {
+        checkAnswer()
+        questionIndex += 1
+        updateUI()
+        
+    }
+    
+    func popUp() {
+        let alert = UIAlertController(title: "Goob job!", message: "You have finished the quiz. Do you want to start over?", preferredStyle: .alert);
+        let restartAction = UIAlertAction(title: "restart", style: .default, handler:
+        { (UIAlertAction) in
+            self.startOver()
+        })
+        alert.addAction(restartAction)
+        present(alert, animated: true, completion: nil)
+    }
     
     func startOver() {
-        questionNr = 0
-        questionLabel.text = questions.list[questionNr].questionText
+        currentPoint = 0
+        questionIndex = 0
+        updateUI()
     }
     
-
+    func printLog() {
+        let tmp : Bool = currentQuestion.answer
+        print("Index: \(questionIndex) - Answer: \(pickedAnswer) - Correct Answer: \(tmp)")
+    }
     
 }
